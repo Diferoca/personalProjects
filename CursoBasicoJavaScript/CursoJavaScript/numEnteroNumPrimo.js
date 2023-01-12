7fb7e38d8f3a7dfc476cc75649ef83b135add9fa
DiegoRodriguez <diferoca30101978@gmail.com>
2023-01-12 09:26:36 -0500

    modified with last change

--- CursoBasicoJavaScript/CursoJavaScript/numEnteroNumPrimo.js
+++ CursoBasicoJavaScript/CursoJavaScript/numEnteroNumPrimo.js
@@ -19,7 +19,19 @@ function esTero(nume) {
     return (Number.isInteger(nume));
  }esTero(1);
 
-
-
 //* codigo para saber si eun numero es primo.
+//! [Nota] 1]: un número primo sólo es divisible por sí mismo y por 1.
+//! [Nota 2]: Debemos utilizar un `bucle for`.
+//! [Nota 3]: los números negativos, 0 y 1 NO son números primos.
+// entonces mediante el bucle for le indicamos al metodo que la variable i inicie por defecto en 2 ya que los numeros 0 y 1 no son primos, segundo con el metodo if validamos si el numero es divisible por si mismo o por 1 con el operador Modulo(%) e indicandole que el sobrante debe ser extrictamente igual a cero(0).     
+
+function esPrimo(num) {
+   
+    if (num <= 1){return false}
+    for (let i = 2; i < num; i ++){
+       if (num % i === 0){
+          return false;
+       }
+    } return true;
+ }esPrimo (7);
 
