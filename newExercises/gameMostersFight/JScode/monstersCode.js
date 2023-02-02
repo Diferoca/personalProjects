b61fc4ecd2adb762fb167229328775046f02fc9e
DiegoRodriguez <diferoca30101978@gmail.com>
2023-02-02 08:12:36 -0500

    add

--- newExercises/gameMostersFight/JScode/monstersCode.js
+++ newExercises/gameMostersFight/JScode/monstersCode.js
@@ -4,31 +4,54 @@ function iniciarjuego() {
   bottonMosterPlayer.addEventListener('click', selectMosterPlayer);
 }
 
+function selectMosterPlayer (){
+
+  let inputTsunami = document.getElementById('Tsunami')
+  let inputEarthquake = document.getElementById('Earthquake')
+  let inputFirewave = document.getElementById('Firewave')
+  let inputCloneboth0 = document.getElementById('Cloneboth0')
+  let spanMonsterPlayer = document.getElementById('monsterPlayer')
+  
 // TODO: The property checked is used for ckeck if the id was selected by the user. 
 
-function selectMosterPlayer (){
-  if (document.getElementById('Tsunami').checked){
-    alert('You have selected Tsunami')
+  if (inputTsunami.checked){
+    spanMonsterPlayer.innerHTML = 'Tsunami'
   }
-  else if (document.getElementById('Earthquake').checked){
-    alert('You have selected Earthquake')
+  else if (inputEarthquake.checked){
+    spanMonsterPlayer.innerHTML = 'Earthquake'
   }
-  else if (document.getElementById('Firewave').checked){
-    alert('You have selected Firewave')
+  else if (inputFirewave.checked){
+    spanMonsterPlayer.innerHTML = 'Firewave' 
   }
-  else if (document.getElementById('Cloneboth').checked){
-    alert('You have selected Cloneboth')
-  }
-  else if (document.getElementById('Cloneboth+').checked){
-    alert('You have selected Cloneboth+')
-  }
-  else if (document.getElementById('Cloneboth++').checked){
-    alert('You have selected Cloneboth++')
+  else if (inputCloneboth0.checked){
+    spanMonsterPlayer.innerHTML = 'Cloneboth0'
   }
   else {
-    alert('You did not select any mosnter')
+    alert('You did not select any monster')
   }
+  selectMosterOpponent();
 }
 
+function selectMosterOpponent (){
+  function numRandom(min, max) {
+    Math.round(Math.random()*(max-min+1))+min;
+  }
+  let opponentAtack = numRandom (1, 4);
+  let spanMonsterOpponent = document.getElementById('monsterOpponent');
+
+  if (opponentAtack == 1){
+    spanMonsterOpponent.innerHTML = 'Tsunami';
+  }
+  else if(opponentAtack == 2){
+    spanMonsterOpponent.innerHTML = 'Earthquake';
+  }
+  else if(opponentAtack == 3){
+    spanMonsterOpponent.innerHTML = 'Firewave';
+  }
+  else{
+    spanMonsterOpponent.innerHTML = 'Cloneboth0';
+  }
+  
+}
 
 window.addEventListener('load', iniciarjuego) //This attribute can be used for tell to browser that load first the html document without matter where is the position of our script in into html document. 
