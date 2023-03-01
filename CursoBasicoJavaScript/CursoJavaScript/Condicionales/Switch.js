ee303d968bf47ce234cd6f42c9e8de255d628b5f
DiegoRodriguez <diferoca30101978@gmail.com>
2023-03-01 12:32:01 -0500

    added Switch.js and fro.js files

--- CursoBasicoJavaScript/CursoJavaScript/Condicionales/Switch.js
+++ CursoBasicoJavaScript/CursoJavaScript/Condicionales/Switch.js
@@ -15,3 +15,37 @@ switch (true)
         console.log("Iguales");break;
     default : console.log('computer win'); 
 }
+
+/* If you have multiple inputs with the same output, you can represent them in a switch statement like this: */
+
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+switch (card) {
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+    count ++;
+    break;
+  case 10:
+  case 'J':
+  case 'Q':
+  case 'K':
+  case 'A':
+    count --;
+    break;
+}
+
+let betHold = 'Hold';
+if (count > 0) {
+  betHold = 'Bet';
+}
+
+console.log(`${count} ${betHold}`);
+
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
--- CursoBasicoJavaScript/CursoJavaScript/Loops/for.js
+++ CursoBasicoJavaScript/CursoJavaScript/Loops/for.js
@@ -1,4 +1,4 @@
-/* Loop for to make a matrix of Odd numbers */
+/* Loop for to make an array with Odd numbers */
 let i = 1;
 let myOddNumbers = [];
 
@@ -8,7 +8,7 @@ for (i; i < 20; i += 2) {
 }
 console.log(myOddNumbers);
 
-/* Loop for to create a matrix Odd numbers backwards*/
+/* Loop for to create an array with Odd numbers backwards*/
 
 let contBackwards = [];
 let total = 0;
@@ -18,9 +18,10 @@ for (let i = 11; i > 0; i -= 2) {
 }
 console.log(contBackwards);
 
+/* Lopp for to add up each number with the next number that contain our array */
+
 for (let i = 0; i < contBackwards.length; i++) {
   total += contBackwards[i];
-  
 }
 console.log(total)
 
